f296d62b8a37e7274cac358f655b14ff88465a2b
alottabeans <50392507+alottabeans@users.noreply.github.com>
2021-12-26 21:46:06 -0800

    Add files via upload

--- snake.c
+++ snake.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <conio.h>
+#include <time.h>
 
 #define UNIX    1
 #define WINDOWS 2
@@ -16,6 +17,7 @@
 #define KEYDOWN  80
 #define KEYLEFT  75
 #define KEYRIGHT 77
+#define ESCAPE   27
 
 
 typedef struct Grid {
@@ -27,18 +29,40 @@ typedef struct Grid {
 
 typedef struct Snake {
   int xpos;
-  int ypos;
-  
-  char sprite;
-  
+  int ypos;  
+  char sprite;  
+
 }Snake;
 
 
 Grid grid;
 Snake snake;
 
+void read_input(Grid* grid, Snake* snake) {
+  char c = getch();
 
-void handle_input(Snake* snake); 
+  switch(c) {
+  case ESCAPE:
+    exit(0);
+    break;
+    
+  case KEYUP:
+    grid->tiles[snake->ypos -= 1][snake->xpos] = 'o';
+    break;
+    
+  case KEYDOWN:
+    grid->tiles[snake->ypos += 1][snake->xpos] = 'o';
+    break;
+
+  case KEYLEFT:
+    grid->tiles[snake->ypos][snake->xpos -= 1] = 'o';
+    break;
+
+  case KEYRIGHT:
+    grid->tiles[snake->ypos][snake->xpos += 1] = 'o';
+    break;
+  }
+}
 
 
 char swap_chars(char current, char new) {
@@ -46,14 +70,13 @@ char swap_chars(char current, char new) {
     return new;
   }
 }
- 
+
 
 void create_grid(Grid* grid) {
   grid->size = sizeof(HEIGHT * WIDTH);
 
   for (int i=0; i <= HEIGHT; i++) {
     for (int j=0; j <= WIDTH; j++) {
-
       grid->tiles[i][j] = '-';
     }
   }
@@ -80,21 +103,80 @@ void draw_grid(Grid* grid) {
   }
 }
 
-void update_snake(Grid* grid, Snake* snake, bool is_running) {
-  grid->tiles[snake->ypos][snake->xpos += 1] = 'o';
   
-  // basic non-char repeating snake code 
-  if (grid->tiles[snake->ypos][snake->xpos - 1] == 'o') {
-    grid->tiles[snake->ypos][snake->xpos - 1] = '-';
+/*
+void update_grid(Grid* grid, Snake* snake) {
+  int score = 0;
+  time_t t;
+  srand((unsigned) time(&t));
+
+  //int random_ypos = rand() % HEIGHT;
+  //int random_xpos = rand() % WIDTH;
+  
+  int random_ypos = 0;
+  int random_xpos = 8;
+  
+  grid->tiles[random_ypos][random_xpos] = '%';
+
+  // check if snake overlaps with apple
+  if (grid->tiles[snake->ypos][snake->xpos] == grid->tiles[random_ypos][random_xpos]) {
+    score++;
+    //grid->tiles[random_ypos][random_xpos] = '-';
+    //grid->tiles[snake->ypos][snake->xpos - score] == 'o';
+  }
+}
+*/
+
+
+void update_snake(Grid* grid, Snake* snake) {
+  int score = 1;
+  
+  //time_t t;
+  //srand(time(NULL));
+  
+  //int random_ypos = rand() % HEIGHT;
+  //int random_xpos = rand() % WIDTH;
+  
+  int random_ypos = 2;
+  int random_xpos = 8;
+  
+  grid->tiles[random_ypos][random_xpos] = '%';
+
+  // check if snake overlaps with apple
+  if (grid->tiles[snake->ypos][snake->xpos] == grid->tiles[random_ypos][random_xpos]) {
+    score++;
+    grid->tiles[random_ypos][random_xpos] = '-';
+    grid->tiles[snake->ypos][snake->xpos - score] == 'o';
+  }
+
+  //grid->tiles[snake->ypos][snake->xpos += 1] = 'o';
+  
+  // overwrite leftover snake symbol from left to right
+  if (grid->tiles[snake->ypos][snake->xpos - score] == snake->sprite) {
+    grid->tiles[snake->ypos][snake->xpos - score] = '-';
+  }
+
+  // overwrite leftover snake symbol from up to bottom
+  if (grid->tiles[snake->ypos - score][snake->xpos] == snake->sprite) {
+    grid->tiles[snake->ypos - score][snake->xpos] = '-';
+  }
+
+  // overwrite leftover snake symbol from right to left
+  if (grid->tiles[snake->ypos][snake->xpos + score] == snake->sprite) {
+    grid->tiles[snake->ypos][snake->xpos + score] = '-';
   }
   
-  if (snake->ypos >= HEIGHT || snake->ypos < 0) {
+  // overwrite leftover snake symbol from bottom to up
+  if (grid->tiles[snake->ypos + score][snake->xpos] == snake->sprite) {
+    grid->tiles[snake->ypos + score][snake->xpos] = '-';
+  }
+		  
+  if (snake->ypos > HEIGHT + 1 || snake->ypos < 0) {
     printf("Game over\n");
     exit(0);
   }
 
-  // cause game over later on 
-  if (snake->xpos >= WIDTH || snake->xpos < 0 ) {
+  if (snake->xpos > WIDTH + 1 || snake->xpos < 0 ) {
     printf("Game over\n");
     exit(0);
   }
@@ -107,7 +189,8 @@ void mainloop(bool is_running) {
   
   while(is_running == true) {
     draw_grid(&grid);
-    update_snake(&grid, &snake, is_running);
+    read_input(&grid, &snake);
+    update_snake(&grid, &snake);
     system("cls");
   }
 }
